ede96e376919f1eb8237363e85c502698aa63d4b
Valteir <valteir.ea@hotmail.com>
2021-07-22 11:02:31 -0300

    login via senha

---
+++ src/main/java/br/com/zupacademy/valteir/proposta/config/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.zupacademy.valteir.proposta.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_propostas:read")
+                        .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_propostas:write")
+                        .antMatchers(HttpMethod.POST, "/biometrias/**").hasAuthority("SCOPE_biometrias:write")
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
